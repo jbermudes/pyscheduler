50516c8587edd58e8572082a9159c9a73a5c7b48
Jess Bermudes <jbermudes@gmail.com>
2015-04-05 23:16:29 -0700

    Fixed whitespace for count_kids and entry; added proper loading of act in entry; Added dancer roster with list of acts to count_kids

--- count_kids.py
+++ count_kids.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function, division
+
 import argparse
 import csv
 import itertools
@@ -11,75 +13,109 @@ from pprint import pprint
 
 
 def load_entries(filename, my_delimiter='\t'):
-  with open(filename, 'rb') as csvfile:
-    tsvreader = csv.DictReader(csvfile, delimiter=my_delimiter)
-    entries = []
-    for row in tsvreader:
-      dancers_str = row["dancer_roster"]
-      dancers = dancers_str.split(',')
-      dancers = [x.strip() for x in dancers]
-      entry = Entry(row, dancers)
-      entries.append(entry)
-  return entries
+    with open(filename, 'rb') as csvfile:
+        tsvreader = csv.DictReader(csvfile, delimiter=my_delimiter)
+        entries = []
+        for row in tsvreader:
+            dancers_str = row["dancer_roster"]
+            dancers = dancers_str.split(',')
+            dancers = [x.strip() for x in dancers]
+            entry = Entry(row, dancers)
+            entries.append(entry)
+    return entries
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="CSV file to use as input")
+    parser.add_argument("prefix", help="prefix for output files")
+    parser.add_argument("-t", "--tab", help="use Tab delimiter instead", action='store_true')
+    args = parser.parse_args()
+
+    use_tab = args.tab
+    filename = args.file
+    out_prefix = args.prefix
+    studio_roster_filename = str(out_prefix) + "_studio_roster.csv"
+    dancer_roster_filename = str(out_prefix) + "_dancer_roster.csv"
+    summary_filename = str(out_prefix) + "_summary.txt"
+    #print "Working with file %s" % (filename)
+
+    print("Working Directory is %s" % os.getcwd())
+
+    if use_tab:
+        delimiter = '\t'
+    else:
+        delimiter = ','
+      
+    entries = load_entries(filename)
+
+    # all_dances["Bob"] = [num_nongroups, num_groups, total, routines]
+    all_dancers = {}
+    studios = {}
+    total_group_dancers = 0 
+    studio_counts = []
+
+    for entry in entries:
+        is_group = entry.is_group()
+
+        if not studios.has_key(entry.studio):
+            studios[entry.studio] = set()
+        studios[entry.studio].update(entry.dancers)
+
+        for dancer in entry.dancers:
+            #print "%s | %s $" % (dancer, entry.studio)
+            if not all_dancers.has_key(dancer):
+              all_dancers[dancer] = [0,0,0,[]]
+            all_dancers[dancer][2] += 1
+            all_dancers[dancer][3].append(entry)
+            if is_group:
+                all_dancers[dancer][1] += 1
+            else:
+                all_dancers[dancer][0] += 1
+
+        if is_group:
+            total_group_dancers += len(entry.dancers)
+
+
+    # Write Dancer Roster by Studio
+    print("Writing Dancer Roster by Studio")
+
+    with open(studio_roster_filename, "w") as f_studio:
+        print("Studio,Dancer,Non-Groups,Groups,Total", file=f_studio)
+        for studio_name in studios:
+            for dancer in studios[studio_name]:
+              dancer_data = all_dancers[dancer]
+              print("%s,%s,%d,%d,%d" % (studio_name, dancer, dancer_data[0], 
+                    dancer_data[1], dancer_data[2]), file=f_studio)
+            studio_counts.append("%s, %d" % (studio_name, len(studios[studio_name])))
+
+        print("", file=f_studio)
+
+    # Write Dancer Roster with each dancer's routines
+    print("Writing Dancer Roster with Routines")
+
+    with open(dancer_roster_filename, "w") as f_dancer:
+        print("Dancer,Act", file=f_dancer)
+        for studio_name in studios:
+            for dancer in studios[studio_name]:
+              dancer_data = all_dancers[dancer]
+              for entry in dancer_data[3]:
+                  print("%s, %s" % (dancer, entry.act), file=f_dancer)
+        print("", file=f_dancer)        
+
+    #Write Summary File with Studio totals and award count estimates
+    print("Writing Summary File")
+
+    with open(summary_filename, "w") as f_summary:
+        print("Studio Totals:", file=f_summary)
+        for line in studio_counts:
+            print(line, file=f_summary)
+
+        print("\nEvent Totals:", file=f_summary)
+        print("Total Unique Dancers at Event: %d" % len(all_dancers), file=f_summary)
+        print("Total Group pins: %d" % total_group_dancers, file=f_summary)
+        print("", file=f_summary)        
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("file", help="CSV file to use as input")
-  parser.add_argument("-t", "--tab", help="use Tab delimiter instead", action='store_true')
-  args = parser.parse_args()
-
-  use_tab = args.tab
-  filename = args.file
-  #print "Working with file %s" % (filename)
-
-  if use_tab:
-    delimiter = '\t'
-  else:
-    delimiter = ','
-      
-  entries = load_entries(filename)
-  all_dancers = {}
-  studios = {}
-  total_group_dancers = 0 
-  studio_counts = []
-
-  for entry in entries:
-    is_group = entry.is_group()
-
-    if not studios.has_key(entry.studio):
-      studios[entry.studio] = set()
-    studios[entry.studio].update(entry.dancers)
-
-    for dancer in entry.dancers:
-      #print "%s | %s $" % (dancer, entry.studio)
-      if not all_dancers.has_key(dancer):
-        all_dancers[dancer] = [0,0,0]
-      all_dancers[dancer][2] += 1
-      if is_group:
-        all_dancers[dancer][1] += 1
-      else:
-        all_dancers[dancer][0] += 1
-
-    if is_group:
-      total_group_dancers += len(entry.dancers)
-
-  print "Studio,Dancer,Non-Groups,Groups,Total"
-  for studio_name in studios:
-    for dancer in studios[studio_name]:
-      dancer_data = all_dancers[dancer]
-      print "%s,%s,%d,%d,%d" % (studio_name, dancer, dancer_data[0], 
-              dancer_data[1], dancer_data[2])
-    studio_counts.append("%s, %d" % (studio_name, len(studios[studio_name])))
-
-  print
-
-  print "\nStudio Totals:"
-  for line in studio_counts:
-    print line
-
-  print "\nEvent Totals:"
-  print "Total Unique Dancers at Event: %d" % len(all_dancers)
-  print "Total Group pins: %d" % total_group_dancers
-
+    main()
 
--- entry.py
+++ entry.py
@@ -1,64 +1,68 @@
 class Entry:
 
-  def __init__(self, dic, dancers):
-    self.entry_id = dic["entry_id"]
-    self.act = ""
-    self.studio = dic["studio"]
-    self.entry_type = "Dance Routine"
-    self.category = dic["entry_grouping"]
-    self.style = dic["style"]
-    self.routine_name = dic["routine_name"]
-    self.duration = dic["duration"]
-    self.num_dancers = dic["num_dancers"]
-    self.level = dic["adj_level"]
-    self.age = dic["age_group"]
-    self.title = dic["add_title"]
-    self.extended = dic["extended"]
-    self.adjudication_only = dic["adjudication_only"]
-    self.event = dic["event"]
-    self.dancers = dancers
+    def __init__(self, dic, dancers):
+        self.entry_id = dic["entry_id"]
+        if dic.has_key("act"):
+            self.act = dic["act"]
+        else:
+            self.act = ""
+
+        self.studio = dic["studio"]
+        self.entry_type = "Dance Routine"
+        self.category = dic["entry_grouping"]
+        self.style = dic["style"]
+        self.routine_name = dic["routine_name"]
+        self.duration = dic["duration"]
+        self.num_dancers = dic["num_dancers"]
+        self.level = dic["adj_level"]
+        self.age = dic["age_group"]
+        self.title = dic["add_title"]
+        self.extended = dic["extended"]
+        self.adjudication_only = dic["adjudication_only"]
+        self.event = dic["event"]
+        self.dancers = dancers
 
 
-  def is_group(self):
-    return ("group" in self.category.lower() or "production" in self.category.lower())
+    def is_group(self):
+        return ("group" in self.category.lower() or "production" in self.category.lower())
 
-  def to_csv(self, delimiter):
-    dancers_str = ", ".join(self.dancers)
-    s = "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"
-    s = s.replace(",", delimiter)
-    s = s % (
-        self.entry_id, self.act, 
-        self.routine_name, self.studio,  
-        self.age, self.category, self.level,
-        self.style,
-        self.duration, self.num_dancers, 
-        self.title, self.extended, self.adjudication_only,
-        self.event, dancers_str)
-    return s
+    def to_csv(self, delimiter):
+        dancers_str = ", ".join(self.dancers)
+        s = "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"
+        s = s.replace(",", delimiter)
+        s = s % (
+            self.entry_id, self.act, 
+            self.routine_name, self.studio,  
+            self.age, self.category, self.level,
+            self.style,
+            self.duration, self.num_dancers, 
+            self.title, self.extended, self.adjudication_only,
+            self.event, dancers_str)
+        return s
 
-  @staticmethod
-  def csv_header(delimiter=','):
-    s = "entry_id,act,routine_name,studio,age_group,entry_grouping,adj_level," \
-        "style,duration," \
-        "num_dancers,add_title,extended,adjudication_only,"\
-        "event,dancer_roster\n"
-    if delimiter != ",":
-      s = s.replace(",", delimiter)
-    
-    return s
-  
-  def __str__(self):
-    s = "Entry Id#%s (%s)" % (str(self.entry_id), str(self.routine_name))
-    return s
+    @staticmethod
+    def csv_header(delimiter=','):
+        s = "entry_id,act,routine_name,studio,age_group,entry_grouping,adj_level," \
+            "style,duration," \
+            "num_dancers,add_title,extended,adjudication_only,"\
+            "event,dancer_roster\n"
+        if delimiter != ",":
+            s = s.replace(",", delimiter)
 
-  def __repr__(self):
-    s = "Entry Id#%s" % self.entry_id
-    return s
+        return s
 
-  def __eq__(self, other):
-    s1 = set(self.dancers)
-    s2 = set(other.dancers)
-    intersection = s1.intersection(s2)
-    return len(intersection) != 0
+    def __str__(self):
+        s = "Entry Id#%s (%s)" % (str(self.entry_id), str(self.routine_name))
+        return s
+
+    def __repr__(self):
+        s = "Entry Id#%s" % self.entry_id
+        return s
+
+    def __eq__(self, other):
+        s1 = set(self.dancers)
+        s2 = set(other.dancers)
+        intersection = s1.intersection(s2)
+        return len(intersection) != 0
 
 
